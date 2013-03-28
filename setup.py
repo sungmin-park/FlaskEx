d4f696ca07409277d4c0011652fa1f3f18faa774
Sung Min Park <vamf12@gmail.com>
2013-03-28 19:43:03 +0900

    update version number for remove Egg.info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='FlaskEx',
-    version='0.0.1',
+    version='0.0.2',
     packages=['flaskex'],
     install_requires=[
         'Flask', 'pyjade', 'pytz', 'iso8601'
