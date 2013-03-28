da54a67bd1914c1772104fd8a8e7cd4524031fb0
Sung Min Park <vamf12@gmail.com>
2013-03-28 20:08:00 +0900

    add pyjade & debugtoolbar support

--- flaskex/__init__.py
+++ flaskex/__init__.py
@@ -1,4 +1,30 @@
+from os import environ
+from logging import StreamHandler, INFO
+
 from flask import Flask as Flask_
+from flask_debugtoolbar import DebugToolbarExtension
+from pyjade.ext.jinja import PyJadeExtension
 
 class Flask(Flask_):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(Flask, self).__init__(*args, **kwargs)
+        # regist jade
+        self.jinja_env.add_extension(PyJadeExtension)
+        self.jinja_env.pyjade.options['pretty'] = 'JADE_PRETTY' in environ
+        # hook config.from_object
+        _config_from_object = self.config.from_object
+        def config_from_object(*args, **kwargs):
+            _config_from_object(*args, **kwargs)
+            if self.debug:
+                DebugToolbarExtension(self)
+            else:
+                self.logger.addHandler(StreamHandler())
+                self.logger.setLevel(INFO)
+        self.config.from_object = config_from_object
+
+    # auto escape jade files too
+    def select_jinja_autoescape(self, filename):
+        if filename and filename.endswith('.jade'):
+            return True
+        return super(Flask, self).select_jinja_autoescape(filename)
+
--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup
 
 setup(
     name='FlaskEx',
-    version='0.0.2',
+    version='0.0.3',
     packages=['flaskex'],
     install_requires=[
-        'Flask', 'pyjade', 'pytz', 'iso8601'
+        'Flask', 'pyjade', 'pytz', 'iso8601', 'flask-debugtoolbar'
+    ],
+    # supporting jinja autoescape
+    dependency_links = [
+        'https://github.com/vamf12/pyjade/tarball/fix_jinja_attr_escape'
+            '#egg=pyjade-2.0.1'
     ]
 )
