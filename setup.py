51dcb918f17618aec2ad1a8b8e15b078de57caf4
Sung Min Park <vamf12@gmail.com>
2013-04-14 14:53:28 +0900

    upgrade version

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup
 
 setup(
     name='FlaskEx',
-    version='0.0.7',
+    version='0.0.8',
     packages=['flaskex'],
+    zip_safe=False,
     install_requires=[
-        'Flask', 'pyjade', 'pytz', 'iso8601', 'flask-debugtoolbar',
+        'Flask', 'pyjade==2.0.1b', 'pytz', 'iso8601', 'flask-debugtoolbar',
         'facebook-sdk'
     ],
     # supporting jinja autoescape
-    dependency_links = [
+    dependency_links=[
         'https://github.com/vamf12/pyjade/tarball/fix_jinja_attr_escape'
-            '#egg=pyjade-2.0.1'
+        '#egg=pyjade-2.0.1b'
     ]
 )
