b112fa325d74e44492c3e43d7cec02d83bc8a293
Sung Min Park <vamf12@gmail.com>
2013-07-29 21:48:12 +0900

    udpate twitter boostrap

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='FlaskEx',
-    version='0.0.47',
+    version='0.0.48',
     packages=['flaskex', 'flaskex.ex', 'flaskex.sns'],
     include_package_data=True,
     zip_safe=False,
