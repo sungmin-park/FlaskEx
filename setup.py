8e8c0ae66ce43fb21ae19075ca22e4b0969b9b7b
Sung Min Park <vamf12@gmail.com>
2013-09-02 19:57:06 +0900

    limit flask-script version for bugs

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='FlaskEx',
-    version='0.0.52',
+    version='0.0.53',
     packages=['flaskex', 'flaskex.ex', 'flaskex.sns'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Flask', 'Flask-WTF', 'Flask-Script', 'Flask-SQLAlchemy',
+        'Flask', 'Flask-WTF', 'Flask-SQLAlchemy',
         'Flask-Assets', 'alembic', 'pyjade==2.0.1b', 'pytz', 'iso8601',
-        'flask-debugtoolbar', 'facebook-sdk', 'glob2', 'simplejson'
+        'flask-debugtoolbar', 'facebook-sdk', 'glob2', 'simplejson',
+        'Flask-Script<0.6'  # flask-assets dose not support 0.6
     ],
     # supporting jinja autoescape
     dependency_links=[
