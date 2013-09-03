ea609159cd65f7d09d6827c40f66604751c261ee
Sung Min Park <vamf12@gmail.com>
2013-09-03 19:40:28 +0900

    add group filter

--- flaskex/jinja_filters.py
+++ flaskex/jinja_filters.py
@@ -1,4 +1,17 @@
+from math import ceil
+
+
 def dt1(dt):
     return dt.strftime('%Y-%m-%d %H:%M:%S')
+
+
+def group(iterable, colunm_count):
+    li = tuple(iterable)
+    ret = list()
+    for i in range(0, int(ceil(len(li) * 1. / colunm_count))):
+        s = i * colunm_count
+        e = s + colunm_count
+        ret.append(li[s:e])
+    return ret
 
 filters = globals()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='FlaskEx',
-    version='0.0.56',
+    version='0.0.57',
     packages=[
         'flaskex', 'flaskex.ex', 'flaskex.ext', 'flaskex.forms', 'flaskex.sns',
     ],
