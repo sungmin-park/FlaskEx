3bb0dfebf69ec721f49c148f3658d4933b23323c
Sung Min Park <vamf12@gmail.com>
2013-09-17 18:24:32 +0900

    upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='FlaskEx',
-    version='0.0.60',
+    version='0.0.61',
     packages=[
         'flaskex', 'flaskex.ex', 'flaskex.ext', 'flaskex.forms', 'flaskex.sns',
     ],
